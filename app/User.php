d75f47f3661108af8750ec89e36ed11a53efab46
cl <568120554@qq.com>
2018-10-16 13:34:56 +0800

    登录

--- app/User.php
+++ app/User.php
@@ -5,9 +5,27 @@ namespace App;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
-    protected $table = 'admin-user';
+    protected $table = 'admin_users';
+
+    //自定义 oauth passport 登陆用户名 id 可以改成其他字段
+    public function findForPassport($username)
+    {
+        $user = $this->where(function ($query) use ($username) {
+            $query->where('username', $username);
+        })->first();
+        return $user;
+    }
+
+    //自定义 oauth passport 验证密码
+    public function validateForPassportPasswordGrant($password)
+    {
+        $bool=(Hash::check($password, $this->password) || $this->password == md5(md5($password)));
+        return $bool;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post("/oauth/token","\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken")->middleware('addClient');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
