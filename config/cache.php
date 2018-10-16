4bc0aa1257dbbb22c84de0fe2a510d3717df9205
cl <568120554@qq.com>
2018-10-16 17:02:35 +0800

    fix

--- config/cache.php
+++ config/cache.php
@@ -70,7 +70,12 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => 'cache',
+            'connection' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => env('REDIS_DB', 0),
+            ],
         ],
 
     ],
